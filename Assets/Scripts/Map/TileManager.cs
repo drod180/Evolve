3a08ba56ae3f032771a4f0cfe25869bdf476db11
Dan Rodriguez <719drod@gmail.com>
2017-04-18 17:22:24 -0400

    Update world generator

--- Assets/Scripts/Map/TileManager.cs
+++ Assets/Scripts/Map/TileManager.cs
@@ -4,35 +4,78 @@ using UnityEngine;
 
 public class TileManager : MonoBehaviour {
 
-	public Vector2 mapSize;
+	public Vector2 mapSize = new Vector2 (40, 40);
 	public char[,] mapValues;
 	public char[] tileValues;
 
-	public void Start () {
-		mapSize = new Vector2 (30, 30);
-	
-		SetTiles ();
-		buildRiver (2);
-		buildRiver (3);
-		buildRiver (2);
-		printMapArray ();
+	public GameObject tileEmpty;
+	public GameObject tileGrass;
+	public GameObject tileMountain;
+	public GameObject tileWater;
 
+	public void Start () {
+		InvokeRepeating ("buildWorld", 1, 5);
 	}
 
 	private void Update () {
 	}
 
+	private void buildWorld () {
+		SetTiles ();
+		AddTilesToWorld ();
+	}
 	private void SetTiles () {
 		initializeMapVars ((int)mapSize.x, (int)mapSize.y);
-		for (int i = 0; i < mapSize.x; i++) {
-			for (int j = 0; j < mapSize.y; j++) {
-				mapValues[i, j] = '?';
-			}
-		}
+		addGrasslands ();
+		addMountainlands ();
+		addWaterlands ();
+
 	}
 
 	private void AddTilesToWorld () {
-		
+		for (int i = 0; i < mapSize.x; i++) {
+			for (int j = 0; j < mapSize.y; j++) {
+				Vector2 spawnPosition = new Vector2 ((float)i, (float)j);
+				switch (mapValues [i, j]) {
+				case 'G':
+					Instantiate (tileGrass, spawnPosition, transform.rotation);
+					break;
+				case 'M':
+					Instantiate (tileMountain, spawnPosition, transform.rotation);
+					break;
+				case 'W':
+					Instantiate (tileWater, spawnPosition, transform.rotation);
+					break;
+				default:
+					Instantiate (tileEmpty, spawnPosition, transform.rotation);
+					break;
+				}
+
+			}
+		}
+
+	}
+
+	private void addWaterlands() {
+		int riverCount = Random.Range (1, 6);
+		for (int i = 0; i < riverCount; i++) {
+			buildTerrain (Random.Range (2, 4), 'W');
+		}
+	}
+
+	private void addMountainlands() {
+		int mountainCount = Random.Range (1, 5);
+		for (int i = 0; i < mountainCount; i++) {
+			buildTerrain (Random.Range (2, 4), 'M');
+		}
+	}
+
+	private void addGrasslands() {
+		for (int i = 0; i < mapSize.x; i++) {
+			for (int j = 0; j < mapSize.y; j++) {
+				mapValues [i, j] = 'G';
+			}
+		}
 	}
 
 	private void initializeMapVars (int x, int y) {
@@ -40,7 +83,7 @@ public class TileManager : MonoBehaviour {
 		mapValues = new char[x, y];
 	}
 
-	private void buildRiver (int size) {
+	private void buildTerrain (int size, char terrain) {
 		int maxSize = size * 2;
 		int minSize = (int)Mathf.Ceil(size / 2);
 		int currentSize = size;
@@ -50,7 +93,7 @@ public class TileManager : MonoBehaviour {
 		int[] currentPos = new int[2] {Random.Range (0, (int)mapSize.x), Random.Range (0, (int)mapSize.y)};
 
 		for (int i = 0; i < length; i++) {
-			buildRiverPiece (direction [currentDirVal], currentPos, currentSize);
+			buildTerrainPiece (direction [currentDirVal], currentPos, currentSize, terrain);
 			currentDirVal = updateDir (currentDirVal);
 			currentSize = updateSize (currentSize, minSize, maxSize);
 			currentPos = updatePos (direction [currentDirVal], currentPos);
@@ -118,7 +161,7 @@ public class TileManager : MonoBehaviour {
 		return currentDir % 8;
 	}
 
-	private void buildRiverPiece (string dir, int[] coord, int size) {
+	private void buildTerrainPiece (string dir, int[] coord, int size, char terrain) {
 		int[][] piece = new int[size][]; 
 		switch (dir) {
 		case "N":
@@ -180,7 +223,7 @@ public class TileManager : MonoBehaviour {
 		for (int i = 0; i < piece.Length; i++) {
 			if ((coord[0] + piece [i][0] >= 0 && coord[0] + piece [i][0] < (int)mapSize.x) 
 				&& (coord[1] + piece[i][1] >= 0 && coord[1] + piece[i][1] < (int)mapSize.y)) {
-				mapValues[(coord[0] + piece[i][0]), (coord[1] + piece[i][1])] = 'W';
+				mapValues[(coord[0] + piece[i][0]), (coord[1] + piece[i][1])] = terrain;
 			}
 
 		}
